fdafe1744b8fb9916addd5dbd4f277d0d15fff2d
Thalles Vieira <thallesvieira654@gmail.com>
2024-08-16 06:50:12 -0300

    feat: Created UserService

---
+++ src/main/java/com/app/ecoplus/Service/UserService.java
@@ -0,0 +1,12 @@
+package com.app.ecoplus.Service;
+import com.app.ecoplus.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}

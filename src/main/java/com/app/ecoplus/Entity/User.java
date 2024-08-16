fb3c6574caa370c034d03e2968cad0713046f934
LucasTavares <lulucas25@hotmail.com>
2024-08-16 00:31:18 -0300

    refactor: modification in the project pattern feat: created entity user

---
+++ src/main/java/com/app/ecoplus/Entity/User.java
@@ -0,0 +1,58 @@
+package com.app.ecoplus.Entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String Nome;
+    private String Sobrenome;
+    private String Cidade;
+    private int Cep;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getSobrenome() {
+        return Sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        Sobrenome = sobrenome;
+    }
+
+    public int getCep() {
+        return Cep;
+    }
+
+    public void setCep(int cep) {
+        Cep = cep;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+}

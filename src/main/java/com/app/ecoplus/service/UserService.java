1d321eaf1795dfaefbc81c190e2734f53a811a9e
Thalles Vieira <thallesvieira654@gmail.com>
2024-08-18 18:37:31 -0300

    feat: Adding getAllUser, findById and updateUser methods into UserService class

--- src/main/java/com/app/ecoplus/service/UserService.java
+++ src/main/java/com/app/ecoplus/service/UserService.java
@@ -20,6 +20,23 @@ public class UserService {
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
     }
-}
 
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    //Checks if user exists before updating
+
+    public User updateUser(User user) {
+    Optional<User> newUser = findById(user.getId());
+    User existingUser = null;
+    if (newUser.isPresent()) {
+        existingUser = newUser.get();
+        existingUser.setNome(user.getNome());
+        return userRepository.save(existingUser);
+    }else {
+        throw new RuntimeException("User not found with ID:" + user.getId());
+    }
+
+}
 }

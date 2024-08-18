6873b6e018917b6223db348acf693f0dcda81807
Thalles Vieira <thallesvieira654@gmail.com>
2024-08-18 17:14:56 -0300

    Update UserService class and create DTO class

--- src/main/java/com/app/ecoplus/service/UserService.java
+++ src/main/java/com/app/ecoplus/service/UserService.java
@@ -1,12 +1,25 @@
 package com.app.ecoplus.service;
-import com.app.ecoplus.Repository.UserRepository;
+import com.app.ecoplus.entity.User;
+import com.app.ecoplus.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+}
 
 }

6179fd607f61e850b5d30e0ff6352d8e76d790ce
LucasTavares <lulucas25@hotmail.com>
2024-08-21 03:37:45 -0300

    update

--- src/main/java/com/app/ecoplus/entity/User.java
+++ src/main/java/com/app/ecoplus/entity/User.java
@@ -3,10 +3,15 @@ package com.app.ecoplus.entity;
 import java.time.LocalDate;
 
 import jakarta.persistence.*;
+<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+=======
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+>>>>>>> b479bd4d71bfdf48fa87740c487dcc9eb0e471f6
 
 @Entity
 @Table(name = "user")
@@ -19,13 +24,21 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String nomeCompleto;
+    @NotBlank
     private String email;
+    @NotBlank
     private String cidade;
+    @NotBlank
     private String serviçoOferecido;
+    @NotBlank
     private Integer Documento;
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> b479bd4d71bfdf48fa87740c487dcc9eb0e471f6
     
 
 }
